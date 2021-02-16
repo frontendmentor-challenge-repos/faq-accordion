aeb7c3dfe6d2df37ec840c05380c126aff747da0
rr69sport <richardramsua@gmail.com>
2021-02-16 18:14:39 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const faq = document.getElementById('faq');
+const accordion = document.getElementById('accordion');
+
+const questions = Array.from(document.querySelectorAll('.accordion__question'))
+const answers = Array.from(document.querySelectorAll('.accordion__answer'))
+
+const removeClass = (target, classToRemove) => {
+
+    target.forEach(t => {
+        t.classList.remove(classToRemove)
+    })
+}
+
+faq.addEventListener('click', (e) => {
+    console.log(e.target)
+})
+accordion.addEventListener('click', (e) => {
+
+    if (!e.target.classList.contains('accordion__question')) return
+
+    removeClass(questions, 'accordion__question--active')
+    removeClass(answers, 'accordion__answer--active')
+    
+    e.target.classList.add('accordion__question--active')
+    e.target.nextElementSibling.classList.add('accordion__answer--active')
+})
